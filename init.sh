53117809ff15df5629d166d3180bc5f06938f432
Ashish Kanwar <ashishthakur913@gmail.com>
2020-04-08 21:16:20 -0400

    New question

---
+++ init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git init
+git remote add origin git@github.com:ashishthakur913/cheating.git
+git pull origin master
---
+++ startSync.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+GIT=`which git`
+while true; do
+	echo ${REPO_DIR}
+	${GIT} add --all .
+	${GIT} commit -m "New question"
+	${GIT} push origin master
+	sleep 5
+done
